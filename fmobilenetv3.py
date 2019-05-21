2fa3c5b8d1c4e3fe894950aacf3c4433d902cf8c
AmigoCDT <DTChang3359@gmail.com>
2019-05-21 13:59:44 +0800

    fix import confusion

--- fmobilenetv3.py
+++ fmobilenetv3.py
@@ -3,7 +3,7 @@ import mxnet.ndarray as nd
 import mxnet.gluon as gluon
 import mxnet.gluon.nn as nn
 import mxnet.autograd as ag
-import symbol_utils
+# import symbol_utils
 
 
 def make_divisible(x, divisible_by=8):
@@ -283,11 +283,14 @@ class MobileNetV3(nn.HybridBlock):
         return self._layers(x)
 
 
-def get_symbol(num_classes=256):
+def get_symbol(num_classes=256, **kwargs):
     net = MobileNetV3(num_classes, 1)
     data = mx.sym.Variable(name='data')
     data = (data-127.5)
     data = data*0.0078125
     body = net(data)
-    # body = symbol_utils.get_fc1(body, num_classes, "E")
+    fc_classes = kwargs.get("fc_classes", 0) # used in insightface
+    if fc_classes:
+        import symbol_utils # file in insightface/src/symbols/
+        body = symbol_utils.get_fc1(body, num_classes, "E")
     return body
